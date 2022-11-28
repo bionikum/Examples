2dd9af6132f7e517f44c3b2fb66f9ccda353d6e9
Ilya <ilya@bobrikov.ru>
2022-11-28 21:20:53 +0500

    Example 004 Div,  добавил рандомные значения

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 10;
-double numberB = 3;
+﻿double numberA = new Random().Next(1, 85); // Рандомное число 1, 2, 3, ....... 84
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 12); // Рандомное число 1, 2, 3, .......11
+Console.WriteLine(numberB);
 Console.Write(numberA / numberB);
