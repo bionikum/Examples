effe7bdddf0de4f54cc96c0076726b839cdeef04
Ilya <ilya@bobrikov.ru>
2022-11-28 21:41:12 +0500

    Example 006 Giri

---
+++ Example006_Giri/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1;
+int b = 8;
+int c = 2;
+int d = 4;
+int e = 6;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine(max);
