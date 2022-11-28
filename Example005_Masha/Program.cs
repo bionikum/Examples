61a9d41343aad290ee8ff05166f39959fe1a1d23
Ilya <ilya@bobrikov.ru>
2022-11-28 21:33:50 +0500

    Example 005 Masha

---
+++ Example005_Masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Как вас зовут..?");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша..!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
