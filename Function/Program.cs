42c2700af2eec4b9d96ac4fa93186d59f3c2f533
Ilya <ilya@bobrikov.ru>
2022-12-01 15:48:10 +0500

    В папке Function еще раз упростил

--- Function/Program.cs
+++ Function/Program.cs
@@ -15,8 +15,6 @@ int c2 = 90;
 int a3 = 96;
 int b3 = 104;
 int c3 = 74;
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3) );
 Console.WriteLine(max);
