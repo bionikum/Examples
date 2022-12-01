9f3779e650cb609e576726dd929aed76fde48ac1
Ilya <ilya@bobrikov.ru>
2022-12-01 15:45:32 +0500

    Нахождение максимального из 9  в папке Function, упростил

--- Function/Program.cs
+++ Function/Program.cs
@@ -1,20 +1,22 @@
 ﻿Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
 int a1 = 21;
 int b1 = 25;
 int c1 = 134;
 int a2 = 561;
 int b2 = 89;
-int c2 = 9000;
+int c2 = 90;
 int a3 = 96;
 int b3 = 104;
 int c3 = 74;
-int max = a1;
-if(b1 > max) max = b1;
-if(c1 > max) max = c1;
-if(a2 > max) max = a2;
-if(b2 > max) max = b2;
-if(c2 > max) max = c2;
-if(a3 > max) max = a3;
-if(b3 > max) max = b3;
-if(c3 > max) max = c3;
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 Console.WriteLine(max);
